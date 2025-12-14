4f3c61ae5c2aa6e6e9e5731de1a0cd599404fa4c
SuikaP <xst5413@gmail.com>
2025-12-15 02:20:17 +0800

    Convert to PWA

--- PWAtest/ClientApp/public/sw.js
+++ PWAtest/ClientApp/public/sw.js
@@ -8,6 +8,6 @@ self.addEventListener('activate', (event) => {
     console.log('Service Worker activating.');
 });
 
-self.addEventListener('fetch', (event) => {chrome://vivaldi-webui/startpage?section=Speed-dials&background-color=#2f2f2f
+self.addEventListener('fetch', (event) => {
     // 這裡可以加入快取邏輯，目前先直接回傳網絡請求
 });
